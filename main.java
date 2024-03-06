a0c87b131e80ca4a102f42a5152fc5936e6b8ec7
pyyshkin <144374275+pyyshkin@users.noreply.github.com>
2024-03-06 20:28:34 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+      ToyStore store = new ToyStore();
+
+        store.addToy(1, "Toy 1", 10, 20);
+        store.addToy(2, "Toy 2", 5, 10);
+        store.addToy(3, "Toy 3", 20, 70);
+
+        store.setToyWeight(2, 30);
+
+        store.play();
+
+        try {
+            store.getPrizeToy();
+        }
+    }
+}
