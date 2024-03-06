7d0da73cddd5408d3e3bf133faf4fd9dafbd66fc
pyyshkin <144374275+pyyshkin@users.noreply.github.com>
2024-03-06 19:57:24 +0300

    Update toystore.java
    addcode

---
+++ toystore.java
@@ -0,0 +1,21 @@
+public class Toy{
+  private int id;
+  private String name;
+  private double weight;
+
+  public Toy(int id, String name, double weight){
+    this.id = id;
+    this.name = name;
+    this.weight = weight;
+  }
+
+  public int getId(){
+    return id;
+  }
+  public String getName(){
+    return name;
+  }
+  public double getWeight(){
+    return weight;
+  }
+}
